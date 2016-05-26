42baa277b80effab0bdd62cac6c2e6da10a41667
SamZhu414 <hlzhu@cn.ibm.com>
2016-05-26 20:14:13 +0800

    test

--- BlueStarService/src/com/ibm/digital/bean/FormBean.java
+++ BlueStarService/src/com/ibm/digital/bean/FormBean.java
@@ -3,7 +3,7 @@ package com.ibm.digital.bean;
 import java.util.List;
 
 public class FormBean {
-	private String ool;
+	
 	private String operationType;
 	private String productName;
 	private String productTitle;
